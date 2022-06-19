c03708555346963896dc76c80aa121069ace20d0
flywukong <2229306838@qq.com>
2022-06-19 18:56:50 +0800

    temp

--- core/rawdb/worker_pool.go
+++ core/rawdb/worker_pool.go
@@ -85,7 +85,7 @@ func (job *Job) CompareKvRocks() error {
 					fmt.Println("compare key error, key:", keyList[i], "leveldb value:",
 						string(job.Kvbuffer[keyList[i]]), "  vs:", string(valueList[i]))
 
-					if keyList[i] == string(headHeaderKey) || keyList[i] == string(headHeaderKey) ||
+					if keyList[i] == string(headHeaderKey) || keyList[i] == string(headBlockKey) ||
 						keyList[i] == string(headFastBlockKey) || keyList[i] == string(lastPivotKey) {
 						continue
 					}
