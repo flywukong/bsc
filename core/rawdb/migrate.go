46682f40c7d2ceb4302d986629d33ad2861e6fb4
flywukong <2229306838@qq.com>
2022-06-20 11:36:08 +0800

    temp

--- core/rawdb/migrate.go
+++ core/rawdb/migrate.go
@@ -36,8 +36,8 @@ func (job *Job) UploadToKvRocks() error {
 	if job.isAncient {
 		err := KvrocksDB.Put(job.ancientKey, job.ancientValue)
 		if err != nil {
-			fmt.Println("send kv error,", err.Error())
-			panic("task fail")
+			fmt.Println("send ancient kv error,", err.Error())
+			panic("ancient task fail")
 			return err
 		}
 	} else {
