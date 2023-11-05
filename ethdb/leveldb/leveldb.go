917392b67e26939db38dcb1616a0edb5fe5d4947
flywukong <2229306838@qq.com>
2023-11-05 23:55:27 +0800

    fix: fix bug

--- ethdb/leveldb/leveldb.go
+++ ethdb/leveldb/leveldb.go
@@ -191,9 +191,6 @@ func (db *Database) Has(key []byte) (bool, error) {
 
 // Get retrieves the given key if it's present in the key-value store.
 func (db *Database) Get(key []byte) ([]byte, error) {
-	goid := cachemetrics.Goid()
-	cachemetrics.UpdateSyncingRoutineID(goid)
-
 	routeid := cachemetrics.Goid()
 	isSyncMainProcess := cachemetrics.IsSyncMainRoutineID(routeid)
 	start := time.Now()
