34dd862ed7cb58669186deb1e584ecd86ed34dfd
flywukong <2229306838@qq.com>
2023-12-04 19:24:33 +0800

    feat: support split trie command

--- core/rawdb/database.go
+++ core/rawdb/database.go
@@ -765,6 +765,9 @@ func InspectDatabase(db ethdb.Database, keyPrefix, keyStart []byte) error {
 				table.size.String(),
 				fmt.Sprintf("%d", ancient.count()),
 			})
+			if strings.Title(table.name) == "Account.Index" {
+
+			}
 		}
 		total += ancient.size()
 	}
@@ -814,4 +817,110 @@ func ReadChainMetadata(db ethdb.KeyValueStore) [][]string {
 		{"fastTxLookupLimit", pp(ReadFastTxLookupLimit(db))},
 	}
 	return data
+}
+
+func SplitDatabase(db ethdb.Database, newDB ethdb.Database) error {
+	it := db.NewIterator([]byte(""), []byte(""))
+	defer it.Release()
+
+	var (
+		count  int64
+		start  = time.Now()
+		logged = time.Now()
+
+		// Key-value store statistics
+		legacyTries  stat
+		stateLookups stat
+		accountTries stat
+		storageTries stat
+		preimages    stat
+
+		chtTrieNodes   stat
+		bloomTrieNodes stat
+
+		// Meta- and unaccounted data
+		metadata stat
+		// Totals
+		total common.StorageSize
+	)
+	// Inspect key-value database first.
+	for it.Next() {
+		var (
+			key   = it.Key()
+			value = it.Value()
+			size  = common.StorageSize(len(key) + len(it.Value()))
+		)
+		total += size
+		switch {
+
+		case IsLegacyTrieNode(key, it.Value()):
+			legacyTries.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case bytes.HasPrefix(key, stateIDPrefix) && len(key) == len(stateIDPrefix)+common.HashLength:
+			stateLookups.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case IsAccountTrieNode(key):
+			accountTries.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case IsStorageTrieNode(key):
+			storageTries.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case bytes.HasPrefix(key, PreimagePrefix) && len(key) == (len(PreimagePrefix)+common.HashLength):
+			preimages.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case bytes.HasPrefix(key, configPrefix) && len(key) == (len(configPrefix)+common.HashLength):
+			metadata.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case bytes.HasPrefix(key, ChtTablePrefix) ||
+			bytes.HasPrefix(key, ChtIndexTablePrefix) ||
+			bytes.HasPrefix(key, ChtPrefix): // Canonical hash trie
+			chtTrieNodes.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		case bytes.HasPrefix(key, BloomTrieTablePrefix) ||
+			bytes.HasPrefix(key, BloomTrieIndexPrefix) ||
+			bytes.HasPrefix(key, BloomTriePrefix): // Bloomtrie sub
+			bloomTrieNodes.Add(size)
+			newDB.Put(key, value)
+			db.Delete(key)
+		default:
+			for _, meta := range [][]byte{fastTrieProgressKey} {
+				if bytes.Equal(key, meta) {
+					metadata.Add(size)
+					newDB.Put(key, value)
+					break
+				}
+			}
+		}
+		count++
+		if count%1000 == 0 && time.Since(logged) > 8*time.Second {
+			log.Info("Splitting trie database", "count", count, "elapsed", common.PrettyDuration(time.Since(start)))
+			logged = time.Now()
+		}
+	}
+	// Display the database statistic of key-value store.
+	stats := [][]string{
+		{"Key-Value store", "Hash trie nodes", legacyTries.Size(), legacyTries.Count()},
+		{"Key-Value store", "Path trie state lookups", stateLookups.Size(), stateLookups.Count()},
+		{"Key-Value store", "Path trie account nodes", accountTries.Size(), accountTries.Count()},
+		{"Key-Value store", "Path trie storage nodes", storageTries.Size(), storageTries.Count()},
+		{"Key-Value store", "Trie preimages", preimages.Size(), preimages.Count()},
+		{"Key-Value store", "Singleton metadata", metadata.Size(), metadata.Count()},
+		{"Light client", "CHT trie nodes", chtTrieNodes.Size(), chtTrieNodes.Count()},
+		{"Light client", "Bloom trie nodes", bloomTrieNodes.Size(), bloomTrieNodes.Count()},
+	}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Database", "Category", "Size", "Items"})
+	table.SetFooter([]string{"", "Total", total.String(), " "})
+	table.AppendBulk(stats)
+	table.Render()
+
+	return nil
 }
