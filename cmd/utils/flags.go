08b3c63a56e9aeb3b04c465fcb7c6917722e1330
flywukong <2229306838@qq.com>
2024-01-03 11:53:40 +0800

    fix: improve code style

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -2295,7 +2295,7 @@ func SplitTrieDatabase(ctx *cli.Context, stack *node.Node, readonly, disableFree
 		handles = MakeDatabaseHandles(ctx.Int(FDLimitFlag.Name))
 	)
 
-	trieDB, err := stack.OpenDatabaseForTrie("chaindata", cache, handles,
+	trieDB, err := stack.OpenTrieDataBase("chaindata", cache, handles,
 		ctx.String(AncientFlag.Name), "eth/db/chaindata/", false, false, false, false)
 	if err != nil {
 		Fatalf("Could not open trie database: %v", err)
