6168f572a4d97e397a646f26934a3d17debc44f8
flywukong <2229306838@qq.com>
2024-01-03 13:09:31 +0800

    fix: fix validator initialization

--- core/blockchain.go
+++ core/blockchain.go
@@ -388,7 +388,10 @@ func NewBlockChain(db ethdb.Database, cacheConfig *CacheConfig, genesis *Genesis
 	bc.flushInterval.Store(int64(cacheConfig.TrieTimeLimit))
 	bc.forker = NewForkChoice(bc, shouldPreserve)
 	bc.stateCache = state.NewDatabaseWithNodeDB(bc.db, bc.triedb)
-	bc.validator = NewBlockValidator(chainConfig, bc, engine)
+	// validator may already been inited in the EnableBlockValidator function in the option
+	if bc.validator == nil {
+		bc.validator = NewBlockValidator(chainConfig, bc, engine)
+	}
 	bc.prefetcher = NewStatePrefetcher(chainConfig, bc, engine)
 	bc.processor = NewStateProcessor(chainConfig, bc, engine)
 
