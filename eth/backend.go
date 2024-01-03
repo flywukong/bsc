99a4cd365da0f7ce83da1be2ef773b7e27801c59
flywukong <2229306838@qq.com>
2024-01-03 19:07:29 +0800

    fix: fix the logic of new blockchain option

--- eth/backend.go
+++ eth/backend.go
@@ -263,6 +263,9 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 		bcOps = append(bcOps, core.EnableDoubleSignChecker)
 	}
 
+	peers := newPeerSet()
+	bcOps = append(bcOps, core.EnableBlockValidator(chainConfig, eth.engine, config.TriesVerifyMode, peers))
+
 	// if the separated trie db has set, need to new blockchain with the separated trie database
 	if stack.Config().TrieDir != "" {
 		log.Info("trie data dir has setted to ", stack.Config().TrieDir)
@@ -273,10 +276,9 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 			return nil, err
 		}
 		bcOps = append(bcOps, core.EnableSeparateDB(separatedDB))
+		cacheConfig.SeparateTrie = true
 	}
 
-	peers := newPeerSet()
-	bcOps = append(bcOps, core.EnableBlockValidator(chainConfig, eth.engine, config.TriesVerifyMode, peers))
 	eth.blockchain, err = core.NewBlockChain(chainDb, cacheConfig, config.Genesis, &overrides, eth.engine, vmConfig, eth.shouldPreserve, &config.TransactionHistory, bcOps...)
 	if err != nil {
 		return nil, err
