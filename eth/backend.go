b49153d4c153621612292e6157d8e862f4cf053e
flywukong <2229306838@qq.com>
2024-01-05 15:43:22 +0800

    temp fix

--- eth/backend.go
+++ eth/backend.go
@@ -263,7 +263,7 @@ func New(stack *node.Node, config *ethconfig.Config) (*Ethereum, error) {
 
 	// if the separated trie db has set, need to new blockchain with the separated trie database
 	if stack.Config().TrieDir != "" {
-		log.Info("trie data dir has setted to ", stack.Config().TrieDir)
+		log.Info("trie data dir has been set with dir: ", stack.Config().TrieDir)
 		separatedDB, err := stack.OpenTrieDataBase("chaindata", config.DatabaseCache, config.DatabaseHandles,
 			config.DatabaseFreezer, "eth/db/chaindata/", false, false, false, config.PruneAncientData)
 		if err != nil {
