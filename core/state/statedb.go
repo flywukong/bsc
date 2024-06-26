54e156d1c6d471563bfe3cbf5fb5a4e85a7341dc
flywukong <2229306838@qq.com>
2024-06-26 10:15:52 +0800

    fix log

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -759,12 +759,14 @@ func (s *StateDB) getDeletedStateObject(addr common.Address) *stateObject {
 							log.Info("compare cache and difflayer not same", "account", acc,
 								"acc1 info", acc.Balance, "code", common.Bytes2Hex(acc.CodeHash), "root", acc.Root,
 								"Nonce", acc.Nonce, "acc2", "nil")
-						} else if acc2 != nil && acc != nil && acc.Balance != acc2.Balance || string(acc.CodeHash) != string(acc2.CodeHash) ||
-							acc.Nonce != acc2.Nonce || string(acc.Root) != string(acc2.Root) {
-							log.Info("compare cache and difflayer not same", "account", acc,
-								"acc1 info", acc.Balance, "code", common.Bytes2Hex(acc.CodeHash), "root", acc.Root,
-								"Nonce", acc.Nonce, "acc2 info", acc2.Balance, "code", common.Bytes2Hex(acc2.CodeHash), "root", acc2.Root,
-								"Nonce", acc2.Nonce)
+						} else if acc2 != nil && acc != nil {
+							if acc.Balance != acc2.Balance || string(acc.CodeHash) != string(acc2.CodeHash) ||
+								acc.Nonce != acc2.Nonce || string(acc.Root) != string(acc2.Root) {
+								log.Info("compare cache and difflayer not same", "account", acc,
+									"acc1 info", acc.Balance, "code", common.Bytes2Hex(acc.CodeHash), "root", acc.Root,
+									"Nonce", acc.Nonce, "acc2 info", acc2.Balance, "code", common.Bytes2Hex(acc2.CodeHash), "root", acc2.Root,
+									"Nonce", acc2.Nonce)
+							}
 						}
 					} else {
 						log.Error("read compare err", "err", err2, "account", acc,
