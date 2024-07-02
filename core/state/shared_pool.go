fcc5255db98aa0947a30b389dad004da59b9503a
flywukong <2229306838@qq.com>
2024-07-02 16:57:40 +0800

    temp test

--- core/state/shared_pool.go
+++ core/state/shared_pool.go
@@ -59,7 +59,7 @@ func NewCacheAmongBlocks() *CacheAmongBlocks {
 		cacheRoot:     types.EmptyRootHash,
 		accountsCache: lru.NewCache[common.Hash, *types.SlimAccount](10000),
 		//	storagesCache:  lru.NewCache[string, []byte](250000),
-		storagesCache2: lru.NewCache[common.Hash, *fastcache.Cache](2000),
+		storagesCache2: lru.NewCache[common.Hash, *fastcache.Cache](200),
 		// accountsCache2: fastcache.New(10000),
 		// storagesCache: fastcache.New(10000),
 	}
@@ -119,11 +119,12 @@ func (c *CacheAmongBlocks) SetStorage(key string, value []byte) {
 func (c *CacheAmongBlocks) SetStorage2(acc common.Hash, storage common.Hash, value []byte) {
 	if c.storagesCache2.Contains(acc) {
 		cacheMap, _ := c.storagesCache2.Get(acc)
-		innerMap := (*fastcache.Cache)(cacheMap)
-		innerMap.Set(storage.Bytes(), value)
-		c.storagesCache2.Add(acc, innerMap)
+		//innerMap := (*fastcache.Cache)(cacheMap)
+		(*fastcache.Cache)(cacheMap).Set(storage.Bytes(), value)
+		//	c.storagesCache2.Add(acc, innerMap)
 	} else {
-		newMap := fastcache.New(10000)
+		log.Info("add a new fast cache", "account ", acc, "storage ", storage)
+		newMap := fastcache.New(20000)
 		//newMap[storage] = value
 		newMap.Set(storage.Bytes(), value)
 		c.storagesCache2.Add(acc, newMap)
