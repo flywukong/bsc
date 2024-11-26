9ff75f38dd35c6850b44131c8dd150cd229db52f
flywukong <2229306838@qq.com>
2024-11-26 16:01:18 +0800

    temp

--- core/state/snapshot/snapshot.go
+++ core/state/snapshot/snapshot.go
@@ -549,10 +549,6 @@ func (t *Tree) cap(diff *diffLayer, layers int) *diskLayer {
 // The disk layer persistence should be operated in an atomic way. All updates should
 // be discarded if the whole transition if not finished.
 func diffToDisk(bottom *diffLayer) *diskLayer {
-	start := time.Now()
-	defer func() {
-		log.Info("diff to disk cost time", "time", time.Since(start).Milliseconds())
-	}()
 	var (
 		base  = bottom.parent.(*diskLayer)
 		batch = base.diskdb.NewBatch()
@@ -567,7 +563,6 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 	// Put the deletion in the batch writer, flush all updates in the final step.
 
 	rawdb.DeleteSnapshotRoot(batch)
-	log.Info("delte")
 
 	// Mark the original base as stale as we're going to create a new wrapper
 	base.lock.Lock()
@@ -577,8 +572,6 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 	base.stale = true
 	base.lock.Unlock()
 
-	start2 := time.Now()
-	start3 := time.Now()
 	// Destroy all the destructed accounts from the database
 	for hash := range bottom.destructSet {
 		// Skip any account not covered yet by the snapshot
@@ -609,7 +602,7 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 		it.Release()
 	}
 
-	log.Info("destructSet diff to disk  cost time", "time", time.Since(start2).Milliseconds())
+	//log.Info("destructSet diff to disk  cost time", "time", time.Since(start2).Milliseconds())
 	start2 = time.Now()
 
 	// Push all updated accounts into the database
@@ -636,7 +629,7 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 			batch.Reset()
 		}
 	}
-	log.Info("account data  diff to disk  cost time", "time", time.Since(start2).Milliseconds())
+	//log.Info("account data  diff to disk  cost time", "time", time.Since(start2).Milliseconds())
 	// Push all the storage slots into the database
 	start2 = time.Now()
 	for accountHash, storage := range bottom.storageData {
@@ -665,7 +658,7 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 		}
 	}
 
-	log.Info("storage data  diff to disk  cost time", "time", time.Since(start2).Milliseconds())
+	//	log.Info("storage data  diff to disk  cost time", "time", time.Since(start2).Milliseconds())
 	// Update the snapshot block marker and write any remainder data
 
 	rawdb.WriteSnapshotRoot(batch, bottom.root)
@@ -678,7 +671,7 @@ func diffToDisk(bottom *diffLayer) *diskLayer {
 	if err := batch.Write(); err != nil {
 		log.Crit("Failed to write leftover snapshot", "err", err)
 	}
-	log.Info("diff to disk  cost time2", "time", time.Since(start3).Milliseconds(), "ms", "s")
+	//	log.Info("diff to disk  cost time2", "time", time.Since(start3).Milliseconds(), "ms", "s")
 	log.Debug("Journalled disk layer", "root", bottom.root, "complete", base.genMarker == nil)
 	res := &diskLayer{
 		root:       bottom.root,
