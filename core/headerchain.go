902eaa288c8dc039748a94dbb2470c4a1e1409f8
flywukong <2229306838@qq.com>
2024-12-04 17:22:39 +0800

    temp

--- core/headerchain.go
+++ core/headerchain.go
@@ -502,7 +502,7 @@ func (hc *HeaderChain) GetTd(hash common.Hash, number uint64) *big.Int {
 // GetHeader retrieves a block header from the database by hash and number,
 // caching it if found.
 func (hc *HeaderChain) GetHeader(hash common.Hash, number uint64) *types.Header {
-	log.Info("begin get header of block", "block", number)
+	//	log.Info("begin get header of block", "block", number)
 	// Short circuit if the header's already in the cache, retrieve otherwise
 	if header, ok := hc.headerCache.Get(hash); ok {
 		return header
