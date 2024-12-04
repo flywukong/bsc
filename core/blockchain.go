4718069e8d70d16f5c48eb9ec0738a27f7c977b7
flywukong <2229306838@qq.com>
2024-12-04 17:19:03 +0800

    fix

--- core/blockchain.go
+++ core/blockchain.go
@@ -1740,9 +1740,11 @@ func (bc *BlockChain) writeKnownBlock(block *types.Block) error {
 // writeBlockWithState writes block, metadata and corresponding state data to the
 // database.
 func (bc *BlockChain) writeBlockWithState(block *types.Block, receipts []*types.Receipt, state *state.StateDB) error {
+	log.Info("write block with state begin")
 	// Calculate the total difficulty of the block
 	ptd := bc.GetTd(block.ParentHash(), block.NumberU64()-1)
 	if ptd == nil {
+		log.Info("fail to get td finish")
 		state.StopPrefetcher()
 		log.Info("Richard:", "failed to find parent hash", block.ParentHash())
 		return consensus.ErrUnknownAncestor
