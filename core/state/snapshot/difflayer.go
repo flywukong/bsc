ce09d82cfa37c4c360b1c25f0277cd68575c9697
flywukong <2229306838@qq.com>
2024-12-17 15:48:02 +0800

    add metrics of snap read

--- core/state/snapshot/difflayer.go
+++ core/state/snapshot/difflayer.go
@@ -288,6 +288,10 @@ func (dl *diffLayer) Accounts() (map[common.Hash]*types.SlimAccount, error) {
 //
 // Note the returned account is not a copy, please don't modify it.
 func (dl *diffLayer) AccountRLP(hash common.Hash) ([]byte, error) {
+	start := time.Now()
+	defer func() {
+		snapshotAccountReadMeter.UpdateSince(start)
+	}()
 	/*
 		// Check staleness before reaching further.
 		dl.lock.RLock()
@@ -361,6 +365,10 @@ func (dl *diffLayer) accountRLP(hash common.Hash, depth int) ([]byte, error) {
 //
 // Note the returned slot is not a copy, please don't modify it.
 func (dl *diffLayer) Storage(accountHash, storageHash common.Hash) ([]byte, error) {
+	start := time.Now()
+	defer func() {
+		snapshotStorageReadMeter.UpdateSince(start)
+	}()
 	/*
 		// Check the bloom filter first whether there's even a point in reaching into
 		// all the maps in all the layers below
