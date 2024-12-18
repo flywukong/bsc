f552a7a154a74a50bb728c621adebcbf296af49c
flywukong <2229306838@qq.com>
2024-12-18 12:21:38 +0800

    Revert "improve channel size"
    This reverts commit 9bd60f582a75dcc3a2711d2de0a117deb9e9cf62.

--- core/blockchain.go
+++ core/blockchain.go
@@ -395,7 +395,7 @@ func NewBlockChain(db ethdb.Database, cacheConfig *CacheConfig, genesis *Genesis
 		vmConfig:           vmConfig,
 		diffQueue:          prque.New[int64, *types.DiffLayer](nil),
 		diffQueueBuffer:    make(chan *types.DiffLayer),
-		verifyTaskCh:       make(chan *VerifyTask, 128),
+		verifyTaskCh:       make(chan *VerifyTask, 32),
 	}
 	bc.flushInterval.Store(int64(cacheConfig.TrieTimeLimit))
 	bc.forker = NewForkChoice(bc, shouldPreserve)
