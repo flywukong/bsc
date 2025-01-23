3dc5316e018ee0ad3ad6dfb76ea7653b3a77e357
flywukong <2229306838@qq.com>
2025-01-23 18:21:59 +0800

    add log

--- core/state_processor.go
+++ core/state_processor.go
@@ -77,10 +77,10 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 	log.Info("process begin", "height", block.NumberU64())
 	lastBlock := p.chain.GetHeaderByHash(block.ParentHash())
 	if lastBlock == nil {
-		log.Info("fail to get last block","height", block.NumberU64()))
+		log.Info("fail to get last block", "height", block.NumberU64())
 		return nil, errors.New("could not get parent block")
 	} else {
-		log.Info("sucess to get last block","height", block.NumberU64()))
+		log.Info("sucess to get last block", "height", block.NumberU64())
 	}
 	// Handle upgrade build-in system contract code
 	systemcontracts.TryUpdateBuildInSystemContract(p.config, blockNumber, lastBlock.Time, block.Time(), statedb, true)
