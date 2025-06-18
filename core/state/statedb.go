f88c5fe17fd3f8daa6b21f8f3ff21893e09aea67
flywukong <19421226+flywukong@users.noreply.github.com>
2025-06-18 12:15:08 +0800

    remove markmetrics

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -200,19 +200,19 @@ type StateDB struct {
 	StorageDeleted atomic.Int64 // Number of storage slots deleted during the state transition
 
 	// 新增：各 get/set 方法的总耗时统计字段
-	TotalGetBalanceCost     time.Duration
-	TotalGetNonceCost       time.Duration
-	TotalGetStorageRootCost time.Duration
-	TotalGetCodeCost        time.Duration
-	TotalGetCodeSizeCost    time.Duration
-	TotalGetCodeHashCost    time.Duration
-	TotalGetStateCost       time.Duration
-	TotalSetBalanceCost     time.Duration
-	TotalSetNonceCost       time.Duration
-	TotalSetCodeCost        time.Duration
-	TotalSetStateCost       time.Duration
-	TotalEmptyCost          time.Duration
-	TotalExistCost          time.Duration
+	// TotalGetBalanceCost     time.Duration
+	// TotalGetNonceCost       time.Duration
+	// TotalGetStorageRootCost time.Duration
+	// TotalGetCodeCost        time.Duration
+	// TotalGetCodeSizeCost    time.Duration
+	// TotalGetCodeHashCost    time.Duration
+	// TotalGetStateCost       time.Duration
+	// TotalSetBalanceCost     time.Duration
+	// TotalSetNonceCost       time.Duration
+	// TotalSetCodeCost        time.Duration
+	// TotalSetStateCost       time.Duration
+	// TotalEmptyCost          time.Duration
+	// TotalExistCost          time.Duration
 }
 
 // NewWithSharedPool creates a new state with sharedStorge on layer 1.5
@@ -438,24 +438,24 @@ func (s *StateDB) SubRefund(gas uint64) {
 // Exist reports whether the given account address exists in the state.
 // Notably this also returns true for self-destructed accounts.
 func (s *StateDB) Exist(addr common.Address) bool {
-	start := time.Now()
-	defer s.markMetrics(start, false, "Exist")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "Exist")
 	return s.getStateObject(addr) != nil
 }
 
 // Empty returns whether the state object is either non-existent
 // or empty according to the EIP161 specification (balance = nonce = code = 0)
 func (s *StateDB) Empty(addr common.Address) bool {
-	start := time.Now()
-	defer s.markMetrics(start, false, "Empty")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "Empty")
 	so := s.getStateObject(addr)
 	return so == nil || so.empty()
 }
 
 // GetBalance retrieves the balance from the given address or 0 if object not found
 func (s *StateDB) GetBalance(addr common.Address) *uint256.Int {
-	start := time.Now()
-	defer s.markMetrics(start, false, "GetBalance")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "GetBalance")
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.Balance()
@@ -465,8 +465,8 @@ func (s *StateDB) GetBalance(addr common.Address) *uint256.Int {
 
 // GetNonce retrieves the nonce from the given address or 0 if object not found
 func (s *StateDB) GetNonce(addr common.Address) uint64 {
-	start := time.Now()
-	defer s.markMetrics(start, false, "GetNonce")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "GetNonce")
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.Nonce()
@@ -477,8 +477,8 @@ func (s *StateDB) GetNonce(addr common.Address) uint64 {
 // GetStorageRoot retrieves the storage root from the given address or empty
 // if object not found.
 func (s *StateDB) GetStorageRoot(addr common.Address) common.Hash {
-	start := time.Now()
-	defer s.markMetrics(start, false, "GetStorageRoot")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "GetStorageRoot")
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return stateObject.Root()
@@ -492,8 +492,8 @@ func (s *StateDB) TxIndex() int {
 }
 
 func (s *StateDB) GetCode(addr common.Address) []byte {
-	start := time.Now()
-	defer s.markMetrics(start, false, "GetCode")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "GetCode")
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		if s.witness != nil {
@@ -513,8 +513,8 @@ func (s *StateDB) GetRoot(addr common.Address) common.Hash {
 }
 
 func (s *StateDB) GetCodeSize(addr common.Address) int {
-	start := time.Now()
-	defer s.markMetrics(start, false, "GetCodeSize")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "GetCodeSize")
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		if s.witness != nil {
@@ -526,50 +526,13 @@ func (s *StateDB) GetCodeSize(addr common.Address) int {
 }
 
 func (s *StateDB) GetCodeHash(addr common.Address) common.Hash {
-	start := time.Now()
-	defer s.markMetrics(start, false, "GetCodeHash")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "GetCodeHash")
 	stateObject := s.getStateObject(addr)
 	if stateObject != nil {
 		return common.BytesToHash(stateObject.CodeHash())
 	}
 	return common.Hash{}
-}
-
-func (s *StateDB) markMetrics(start time.Time, reachStorage bool, op string) {
-	isSyncMainProcess := cachemetrics.IsSyncMainRoutineID(cachemetrics.Goid())
-	if !isSyncMainProcess {
-		return
-	}
-	elapsed := time.Since(start)
-	s.totalSyncIOCost += elapsed
-	switch op {
-	case "GetBalance":
-		s.TotalGetBalanceCost += elapsed
-	case "GetNonce":
-		s.TotalGetNonceCost += elapsed
-	case "GetStorageRoot":
-		s.TotalGetStorageRootCost += elapsed
-	case "GetCode":
-		s.TotalGetCodeCost += elapsed
-	case "GetCodeSize":
-		s.TotalGetCodeSizeCost += elapsed
-	case "GetCodeHash":
-		s.TotalGetCodeHashCost += elapsed
-	case "GetState":
-		s.TotalGetStateCost += elapsed
-	case "SetBalance":
-		s.TotalSetBalanceCost += elapsed
-	case "SetNonce":
-		s.TotalSetNonceCost += elapsed
-	case "SetCode":
-		s.TotalSetCodeCost += elapsed
-	case "SetState":
-		s.TotalSetStateCost += elapsed
-	case "Empty":
-		s.TotalEmptyCost += elapsed
-	case "Exist":
-		s.TotalExistCost += elapsed
-	}
 }
 
 // GetState retrieves a value from the given account's storage trie.
@@ -583,7 +546,7 @@ func (s *StateDB) GetState(addr common.Address, hash common.Hash) common.Hash {
 			getStatetSyncIOCost.Update(syncGetDelay)
 			l1AccountMeter.Mark(1)
 		}
-		s.markMetrics(start, false, "GetState")
+		// s.markMetrics(start, false, "GetState")
 	}()
 
 	stateObject := s.getStateObject(addr)
@@ -599,14 +562,14 @@ func (s *StateDB) GetState(addr common.Address, hash common.Hash) common.Hash {
 // GetCommittedState retrieves the value associated with the specific key
 // without any mutations caused in the current execution.
 func (s *StateDB) GetCommittedState(addr common.Address, hash common.Hash) common.Hash {
-	start := time.Now()
-	needStorage := false
-	defer s.markMetrics(start, needStorage, "GetCommittedState")
+	// start := time.Now()
+	// needStorage := false
+	// defer s.markMetrics(start, needStorage, "GetCommittedState")
 	stateObject := s.getStateObject(addr)
 	hit := false
 
 	if stateObject != nil {
-		needStorage = true
+		// needStorage = true
 		return stateObject.GetCommittedState(hash, &hit, false)
 	}
 	return common.Hash{}
@@ -631,9 +594,9 @@ func (s *StateDB) HasSelfDestructed(addr common.Address) bool {
 
 // AddBalance adds amount to the account associated with addr.
 func (s *StateDB) AddBalance(addr common.Address, amount *uint256.Int, reason tracing.BalanceChangeReason) uint256.Int {
-	start := time.Now()
+	// start := time.Now()
 	stateObject := s.getOrNewStateObject(addr)
-	s.markMetrics(start, false, "AddBalance")
+	// s.markMetrics(start, false, "AddBalance")
 	if stateObject == nil {
 		return uint256.Int{}
 	}
@@ -642,9 +605,9 @@ func (s *StateDB) AddBalance(addr common.Address, amount *uint256.Int, reason tr
 
 // SubBalance subtracts amount from the account associated with addr.
 func (s *StateDB) SubBalance(addr common.Address, amount *uint256.Int, reason tracing.BalanceChangeReason) uint256.Int {
-	start := time.Now()
+	// start := time.Now()
 	stateObject := s.getOrNewStateObject(addr)
-	s.markMetrics(start, false, "SubBalance")
+	// s.markMetrics(start, false, "SubBalance")
 	if stateObject == nil {
 		return uint256.Int{}
 	}
@@ -655,27 +618,27 @@ func (s *StateDB) SubBalance(addr common.Address, amount *uint256.Int, reason tr
 }
 
 func (s *StateDB) SetBalance(addr common.Address, amount *uint256.Int, reason tracing.BalanceChangeReason) {
-	start := time.Now()
+	// start := time.Now()
 	stateObject := s.getOrNewStateObject(addr)
-	s.markMetrics(start, false, "SetBalance")
+	// s.markMetrics(start, false, "SetBalance")
 	if stateObject != nil {
 		stateObject.SetBalance(amount)
 	}
 }
 
 func (s *StateDB) SetNonce(addr common.Address, nonce uint64, reason tracing.NonceChangeReason) {
-	start := time.Now()
+	// start := time.Now()
 	stateObject := s.getOrNewStateObject(addr)
-	s.markMetrics(start, false, "SetNonce")
+	// s.markMetrics(start, false, "SetNonce")
 	if stateObject != nil {
 		stateObject.SetNonce(nonce)
 	}
 }
 
 func (s *StateDB) SetCode(addr common.Address, code []byte) (prev []byte) {
-	start := time.Now()
+	// start := time.Now()
 	stateObject := s.getOrNewStateObject(addr)
-	s.markMetrics(start, false, "SetCode")
+	// s.markMetrics(start, false, "SetCode")
 	if stateObject != nil {
 		prev = stateObject.SetCode(crypto.Keccak256Hash(code), code)
 	}
@@ -683,9 +646,9 @@ func (s *StateDB) SetCode(addr common.Address, code []byte) (prev []byte) {
 }
 
 func (s *StateDB) SetState(addr common.Address, key, value common.Hash) common.Hash {
-	start := time.Now()
+	// start := time.Now()
 	stateObject := s.getOrNewStateObject(addr)
-	s.markMetrics(start, false, "SetState")
+	// s.markMetrics(start, false, "SetState")
 	if stateObject != nil {
 		return stateObject.SetState(key, value)
 	}
@@ -895,8 +858,8 @@ func (s *StateDB) createObject(addr common.Address) *stateObject {
 // exists, this function will silently overwrite it which might lead to a
 // consensus bug eventually.
 func (s *StateDB) CreateAccount(addr common.Address) {
-	start := time.Now()
-	defer s.markMetrics(start, false, "CreateAccount")
+	// start := time.Now()
+	// defer s.markMetrics(start, false, "CreateAccount")
 	s.createObject(addr)
 }
 
@@ -1053,15 +1016,6 @@ func (s *StateDB) Finalise(deleteEmptyObjects bool) {
 	}
 	// Invalidate journal because reverting across transactions is not allowed.
 	s.clearJournalAndRefund()
-
-	// 新增：清零各 get 方法的总耗时统计字段
-	s.TotalGetBalanceCost = 0
-	s.TotalGetNonceCost = 0
-	s.TotalGetStorageRootCost = 0
-	s.TotalGetCodeCost = 0
-	s.TotalGetCodeSizeCost = 0
-	s.TotalGetCodeHashCost = 0
-	s.TotalGetStateCost = 0
 }
 
 // IntermediateRoot computes the current root hash of the state trie.
