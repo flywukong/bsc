e36c7c41a46d2892a021f685d170ef3fdd48c571
flywukong <19421226+flywukong@users.noreply.github.com>
2025-08-07 16:22:02 +0800

    Temp

--- cmd/state-perf/main.go
+++ cmd/state-perf/main.go
@@ -1326,7 +1326,7 @@ func (r *PerfRunner) initializeTrie() error {
 	var config *triedb.Config
 	if dbScheme == rawdb.PathScheme {
 		config = &triedb.Config{
-			PathDB: utils.PathDBConfigAddJournalFilePath(r.stack, pathdb.ReadOnly),
+			PathDB: utils.PathDBConfigAddJournalFilePath(r.stack, pathdb.Defaults),
 			Cache:  0,
 		}
 	} else if dbScheme == rawdb.HashScheme {
