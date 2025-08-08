90815c1669da1280e8e120f376eb06222de94521
flywukong <19421226+flywukong@users.noreply.github.com>
2025-08-08 22:36:07 +0800

    feat: support separate tx-index database

--- node/node.go
+++ node/node.go
@@ -76,11 +76,13 @@ const (
 	runningState
 	closedState
 	// ChainDbResourcePercentage is estimated from on-disk size proportions of metadata,index data and block data.
-	ChainDbResourcePercentage = 26
+	ChainDbResourcePercentage = 11
 	// SnapDbResourcePercentage is estimated from on-disk size proportions of snapshot data.
 	SnapDbResourcePercentage = 24
 	// StateStoreResourcePercentage is estimated from on-disk size proportions of trie data.
 	StateStoreResourcePercentage = 50
+	// IndexDbResourcePercentage is estimated from on-disk size proportions of transaction index data.
+	IndexDbResourcePercentage = 15
 )
 
 const StateDBNamespace = "eth/db/statedata/"
@@ -795,6 +797,7 @@ func (n *Node) OpenAndMergeDatabase(name string, namespace string, readonly bool
 		chainDbCache                 = config.DatabaseCache
 		stateDbCache, stateDbHandles int
 		snapDbCache, snapDbHandles   int
+		indexDbCache, indexDbHandles int
 	)
 
 	isMultiDatabase := n.CheckIfMultiDataBase()
@@ -809,8 +812,11 @@ func (n *Node) OpenAndMergeDatabase(name string, namespace string, readonly bool
 		snapDbCache = int(float64(config.DatabaseCache) * SnapDbResourcePercentage / 100)
 		snapDbHandles = int(float64(config.DatabaseHandles) * SnapDbResourcePercentage / 100)
 
-		stateDbCache = config.DatabaseCache - chainDbCache - snapDbCache
-		stateDbHandles = config.DatabaseHandles - chainDataHandles - snapDbHandles
+		indexDbCache = int(float64(config.DatabaseCache) * IndexDbResourcePercentage / 100)
+		indexDbHandles = int(float64(config.DatabaseHandles) * IndexDbResourcePercentage / 100)
+
+		stateDbCache = config.DatabaseCache - chainDbCache - snapDbCache - indexDbCache
+		stateDbHandles = config.DatabaseHandles - chainDataHandles - snapDbHandles - indexDbHandles
 	}
 
 	chainDB, err := n.OpenDatabaseWithFreezer(name, chainDbCache, chainDataHandles, config.DatabaseFreezer, namespace, readonly, false)
@@ -835,6 +841,14 @@ func (n *Node) OpenAndMergeDatabase(name string, namespace string, readonly bool
 		}
 
 		chainDB.SetSnapStore(snapshotDb)
+
+		// Open the tx index database as a pure key-value store
+		indexDb, err := n.OpenDatabase(name+"/txindex", indexDbCache, indexDbHandles, "eth/db/txindex/", readonly, true)
+		if err != nil {
+			log.Error("Failed to open separate tx index database", "err", err)
+			return nil, err
+		}
+		chainDB.SetTxIndexStore(indexDb)
 	}
 
 	return chainDB, nil
