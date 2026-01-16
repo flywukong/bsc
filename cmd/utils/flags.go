f49ab6292ecf52d01243285cf4de1baf9e63920b
flywukong <19421226+flywukong@users.noreply.github.com>
2026-01-16 10:30:26 +0800

    fix: improve code

--- cmd/utils/flags.go
+++ cmd/utils/flags.go
@@ -427,7 +427,7 @@ var (
 	// Deprecated Jan 2025
 	LogExportCheckpointsFlag = &cli.StringFlag{
 		Name:     "history.logs.export",
-		Usage:    "Deprecated, checkpoint file is auto-enabled at datadir/geth/filtermap_checkpoints.json",
+		Usage:    "Export checkpoints to file in go source file format",
 		Category: flags.StateCategory,
 	}
 	// Beacon client light sync settings
@@ -2179,7 +2179,6 @@ func SetEthConfig(ctx *cli.Context, stack *node.Node, cfg *ethconfig.Config) {
 	}
 	if ctx.IsSet(LogExportCheckpointsFlag.Name) {
 		cfg.LogExportCheckpoints = ctx.String(LogExportCheckpointsFlag.Name)
-		log.Warn("Flag --history.logs.export is deprecated, checkpoint file is auto-enabled at datadir/geth/filtermap_checkpoints.json")
 	}
 	if ctx.String(GCModeFlag.Name) == "archive" && cfg.BlockHistory != 0 {
 		cfg.BlockHistory = 0
